c7c335d878f51d68e3c20811e1c74850eecf5e8a
sanskar <sanskargupta15@gmail.com>
2023-03-23 20:11:15 +0530

    passing second test?

--- app/main.py
+++ app/main.py
@@ -3,26 +3,33 @@ import os
 import zlib  
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[1]
-    if command == "init":
+    args = sys.argv[2:]
+    if command == 'init':
+         init()
+    elif command == 'cat-file':
+         cat_file()
+    else:
+         raise RuntimeError(f"Unknown command #{command}")
+
+def init():
         os.mkdir(".git")
         os.mkdir(".git/objects")
         os.mkdir(".git/refs")
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
-    else:
-        raise RuntimeError(f"Unknown command #{command}")
 
+def cat_file(*args):
+    _mode, hash = args
+    hash_lead = hash[0:2]
+    hash_tail = hash[2:]
 
-compressed_data = open('.git/objects', 'rb').read()  
-decompressed_data = zlib.decompress(compressed_data)  
-print(decompressed_data)  
+    content = open(".git/objects/{}/{}".format(hash_lead, hash_tail), "rb")
+    content = content.read()
 
-if __name__ == "__main__":
-    main()
+    decompressed = zlib.decompress(content)
+    decompressed = decompressed.decode("utf-8")
+    decompressed = decompressed[8:]
+
+    sys.stdout.write(decompressed)
